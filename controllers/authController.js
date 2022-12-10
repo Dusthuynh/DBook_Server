c6a4403e2a9bdc60320f7bf26c801c898a82b4a3
DustHuynh <huynhdust0411@gmail.com>
2022-12-10 16:51:30 +0700

    fix path Model

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
-const User = require('../models/userModel');
+const User = require('../Models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
