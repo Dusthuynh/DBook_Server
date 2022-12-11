81ee644cf6abf3b3de3817ee7df610aafec0c3aa
DustHuynh <huynhdust0411@gmail.com>
2022-12-11 16:58:51 +0700

    CORS_Enabled

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ app.use((req, res, next) => {
 });
 
 //routes
+app.all('/', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
+});
 app.use('/api/v1/posts', postRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/books', bookRouter);
